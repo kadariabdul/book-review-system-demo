7065534772e9771991ec0520793c89850df8118b
Abdul Saiyad <kadariabdul@gmail.com>
2024-07-07 03:27:50 +0530

    added pm2 config file

---
+++ pm2.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps: [
+        {
+            name: 'book-review-system-server',
+            script: 'dist/index.js',
+            watch: true,
+            env: {
+                NODE_ENV: 'production',
+                PORT: process.env.PORT || 4000,
+            },
+        }
+    ],
+};
